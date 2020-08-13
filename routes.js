799e94bdd31cc75a96955e52aa334a5547cf45a9
Eamonn de Leastar <edeleastar@wit.ie>
2020-08-13 10:55:54 +0100

    routes for edit song

--- routes.js
+++ routes.js
@@ -7,6 +7,7 @@ const accounts = require("./controllers/accounts.js");
 const dashboard = require("./controllers/dashboard.js");
 const about = require("./controllers/about.js");
 const playlist = require("./controllers/playlist.js");
+const song = require("./controllers/song.js");
 
 router.get("/", accounts.index);
 router.get("/login", accounts.login);
@@ -23,5 +24,8 @@ router.get("/about", about.index);
 router.get("/playlist/:id", playlist.index);
 router.get("/playlist/:id/deletesong/:songid", playlist.deleteSong);
 router.post("/playlist/:id/addsong", playlist.addSong);
+
+router.get("/song/:id/editsong/:songid", song.index);
+router.post("/song/:id/updatesong/:songid", song.update);
 
 module.exports = router;
